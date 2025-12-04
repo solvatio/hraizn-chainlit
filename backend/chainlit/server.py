d38bd6c8f9e1efc73267d8718299a5849847142e
Valentin Burger <valentin.burger@solvatio.com>
2025-12-04 15:03:18 +0100

    HRAIZN-1962 feat: add EXCLUDE_ROBOTS_INDEX flag to exclude search engine robots from index

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -396,6 +396,9 @@ def get_html_template(root_path):
     <meta property="og:url" content="{default_url}">
     <meta property="og:root_path" content="{root_path}">"""
 
+    if os.getenv("EXCLUDE_ROBOTS_INDEX", 'False').lower() in ('true', '1', 't', 'yes', 'y'):
+        tags += '\n<meta name="robots" content="noindex, nofollow">'
+
     js = f"""<script>
 {f"window.theme = {json.dumps(custom_theme.get('variables'))};" if custom_theme and custom_theme.get("variables") else "undefined"}
 {f"window.transports = {json.dumps(config.project.transports)};" if config.project.transports else "undefined"}
